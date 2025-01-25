56b4eecd7019e15137ff6d4d238290eb5ae2f9a5
Praveen Kumar <140703780+raopraveen0@users.noreply.github.com>
2025-01-26 02:04:25 +0530

    update

--- src/api/allApi.js
+++ src/api/allApi.js
@@ -3,7 +3,6 @@ import axios from "axios";
 const allApi = {
     weatherApi: async (city) => {
         try {
-            debugger;
             const API_WEATHER_KEY = import.meta.env.VITE_API_WEATHER_KEY
             const Response = await axios.get(`https://api.openweathermap.org/data/2.5/weather?q=${city}&appid=${API_WEATHER_KEY}&units=metric`);
             return Response.data;
