33bd8be8f4487aedad91044887332748a55f234b
Praveen Kumar <140703780+raopraveen0@users.noreply.github.com>
2025-01-26 01:27:59 +0530

    initial commit

---
+++ src/App.jsx
@@ -0,0 +1,17 @@
+
+function App() {
+
+  return (
+    <>
+    <div className="">
+      <h1 className="text-3xl text-center">Praveen Kumar</h1>
+      <h1 class="text-3xl font-bold underline bg-red-500 text-white">
+    Hello world!
+  </h1>
+    </div>
+
+    </>
+  )
+}
+
+export default App
